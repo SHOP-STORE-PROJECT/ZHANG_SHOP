5aac8587391d21fd5cdcaee16afba3c320da0ee0
fanzhangGitHub <2468242266@qq.com>
2016-12-15 16:39:12 +0800

    Merge 12/15

--- app/src/main/java/shop/yunifang/com/yunifang/activity/TheorderActivity.java
+++ app/src/main/java/shop/yunifang/com/yunifang/activity/TheorderActivity.java
@@ -1,26 +1,23 @@
 package shop.yunifang.com.yunifang.activity;
 
-import android.os.Bundle;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-
-import shop.yunifang.com.yunifang.R;
 
 public class TheorderActivity extends AppCompatActivity {
 
     private ViewPager ordervp;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_theorder);
-
-        initView();
-
-    }
-
-    private void initView() {
-    ordervp=(ViewPager)findViewById(R.id.order_vp);
-
-    }
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_theorder);
+//
+//        initView();
+//
+//    }
+//
+//    private void initView() {
+//    ordervp=(ViewPager)findViewById(R.id.order_vp);
+//
+//    }
 }
